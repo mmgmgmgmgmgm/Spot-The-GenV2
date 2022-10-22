a2d1af725da1bcf132b03ee63741732c51990c36
mmgmgmgmgmgm <97559571+mmgmgmgmgmgm@users.noreply.github.com>
2022-10-22 11:50:00 -0400

    Update Head.py

--- Head.py
+++ Head.py
@@ -4,7 +4,7 @@ import requests, string, random, argparse, sys, os, time
 os.system('cls')
 
 name =  Write.Input("---------------------------------Name the result file-------------------------------- : ", Colors.blue_to_green, interval=0.01)
-amount = Write.Input("--------------------------------How many accounts would you like to generate?---------------------------------:", Colors.blue_to_green, interval=0.01)
+amount = Write.Input("--------------------------------How many accounts would you like to generate(Please give credit to Ishaan)?---------------------------------:", Colors.blue_to_green, interval=0.01)
 file_path = "main.py"
 print(Box.DoubleCube(Colorate.Horizontal(Colors.blue_to_green, "Results will be saved in " + (name) + ".txt")))
 #python spotify_generator.py -n AMOUNT -o OUTPUT_FILE
